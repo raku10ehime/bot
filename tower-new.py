80a0d8afd9beb4376d3e2ef0e65f9447cc785770
raku10ehime <95738593+raku10ehime@users.noreply.github.com>
2021-12-24 00:45:51 +0900

    Update tower-new.py

--- tower-new.py
+++ tower-new.py
@@ -1,10 +1,30 @@
 import datetime
 import os
+import time
 
-import pandas as pd
-import tweepy
 import geopandas as gpd
+import japanize_matplotlib
 import matplotlib.pyplot as plt
+import pandas as pd
+import requests
+import tweepy
+
+
+def get_address(se):
+
+    r = requests.get(
+        "https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress",
+        params={"lat": se["lat"], "lon": se["lon"]},
+    )
+
+    time.sleep(1)
+
+    data = r.json()
+
+    result = data.setdefault("results", {}).get("lv01Nm", "")
+
+    return result
+
 
 url = "https://mls.js2hgw.com/update.php?base_enb=737280"
 
@@ -24,20 +44,22 @@ df0 = pd.read_json(
 df0["area1"].mask(df0["area1"] == "", "不明", inplace=True)
 
 # 経度・緯度
-df0[["lat", "lng"]] = (
+df0[["lat", "lon"]] = (
     df0["location"].str.strip("()").str.split(",", expand=True).astype(float)
 )
 
-df0["場所"] = df0["pref"].str.cat(df0["area1"])
 df0["eNB-LCID"] = df0["enb"].astype(str).str.cat(df0["lcid"].astype(str), "-")
+
 df0["date"] = df0["created"].dt.date
 
-df1 = pd.crosstab(df0["date"], df0["area1"])
+df0["場所"] = df0["pref"].str.cat(df0["area1"])
 
-df2 = df1.reindex(dt_range, fill_value=0)
+df1 = pd.crosstab(df0["date"], df0["area1"]).reindex(dt_range, fill_value=0)
+df1
 
 # 前日
-se = df2.iloc[-2]
+se = df1.iloc[-2]
+
 
 if se.sum():
 
@@ -49,18 +71,37 @@ if se.sum():
     twit += f"\n{url}"
 
     print(twit)
-    
-    df3 = df0[df0["date"] == se.name].copy().sort_values(["enb", "lcid"])
 
-    geo_df = gpd.GeoDataFrame(df3, geometry=gpd.points_from_xy(df3.lng, df3.lat), crs=6668)
+    # 地図
+
+    df2 = df0[df0["date"] == se.name].copy().sort_values(["enb", "lcid"])
+
+    geo_df = gpd.GeoDataFrame(
+        df2, geometry=gpd.points_from_xy(df2.lon, df2.lat), crs=6668
+    )
+
     ehime = gpd.read_file("N03-20210101_38_GML.zip!N03-20210101_38_GML")
 
     base = ehime.plot(color="white", edgecolor="black")
     ax = geo_df.plot(ax=base, marker="o", color="red", markersize=5)
     ax.set_axis_off()
-    
+
     plt.savefig("map.png", dpi=200)
-    
+
+    # テーブル
+
+    df2["area2"] = df2.apply(get_address, axis=1)
+    df2["place"] = df2["area1"].str.cat(df2["area2"])
+
+    df3 = df2["place"].value_counts().rename_axis("place").reset_index(name="counts")
+
+    fig, ax = plt.subplots()
+
+    ax.axis("off")
+
+    ax.table(cellText=df3.values, colLabels=df3.columns, loc="center")
+    plt.savefig("table.png", bbox_inches="tight", dpi=300)
+
     consumer_key = os.environ["CONSUMER_KEY"]
     consumer_secret = os.environ["CONSUMER_SECRET"]
     access_token = os.environ["ACCESS_TOKEN"]
@@ -71,6 +112,8 @@ if se.sum():
     auth.set_access_token(access_token, access_token_secret)
 
     api = tweepy.API(auth)
-    
-    media_id = api.media_upload("map.png").media_id
-    api.update_status(status=twit, media_ids=[media_id])
+
+    map_id = api.media_upload("map.png").media_id
+    table_id = api.media_upload("table.png").media_id
+
+    api.update_status(status=twit, media_ids=[map_id, table_id])
